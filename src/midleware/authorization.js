2a239aa1ae6fb3e3639e53a0d7e3d3adb73f56ca
hoainam201 <mayurijudgement24@gmail.com>
2023-12-16 21:53:28 +0700

    Authorization

---
+++ src/midleware/authorization.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+require("dotenv").config();
+
+module.exports = async (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const user = await User.findOne({
+            where: {
+                id: decoded.id
+            }
+        })
+        if(!user) {
+            res.sendStatus(401);
+        }
+        req.userData = decoded;
+        next();
+    }
+    catch(error) {
+        console.log(error);
+        res.sendStatus(401);
+    }
+}
