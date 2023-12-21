150d37e9add0b339d6df4225821efd2de730d4f4
hoainam201 <mayurijudgement24@gmail.com>
2023-12-21 23:56:18 +0700

    Change language message

--- src/midleware/authorization.js
+++ src/midleware/authorization.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 module.exports = async (req, res, next) => {
     try{
         const token = req.headers.authorization.split(" ")[1];
+        console.log(req.headers.authorization)
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
         const user = await User.findOne({
             where: {
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const siteController = require("../controllers/siteController");
+const siteController = require("../controllers/SiteController");
 
 router.post('/search', siteController.search);
 router.get('/get-ramdom-food', siteController.getRandomFoods);
